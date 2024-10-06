e1dcd5c393e7eb3b65fa9281cb0f181f77b7b958
Alvin Peng <p0987188171@gmail.com>
2024-10-06 15:38:53 +0800

    Version: 0.10, Release date: 2024/10/06

---
+++ Week1/cold_joke.js
@@ -0,0 +1,11 @@
+title = "冷笑話";
+title += ":";
+
+who = "小名"
+where = "超商"
+why = "為甚麼"
+when = "剿匪"
+how = "坐輪椅"
+
+console.log(title + who + why + "去" + where + when + "後"+ how);
+console.log(`${title}${why}${who}去'${where}${when}'後，${who}出來卻要${how}?`);
---
+++ Week1/script.js
@@ -0,0 +1,15 @@
+    month = 10;
+    day = 11;
+    // Doc_Score=87;
+    // Interview_Score=83;
+
+    console.log("Date:", month+"/"+ day);
+    /*if((((Doc_Score * 0.8)+(Interview_Score * 1.2)) / 2) < 87)
+    {
+        console.error("放榜結果: 您挫賽了");
+    }
+    else
+    {
+        console.log("虎尾科大歡迎您");
+    }*/
+   
